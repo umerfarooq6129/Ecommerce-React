1a45315a049331b8ed50de3911375f7dbd776ed1
umerfarooq6129 <umer.uze@gmail.com>
2023-11-19 22:36:30 +0500

    breadcrum

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from './../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/BreadCrum/Breadcrum';
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext);
+  const {prodictId} = useParams();
+  const product = all_product.find((e) => e.id === Number(prodictId))
   return (
-    <div>Product</div>
+    <>
+      <Breadcrum product={product} />
+    </>
   )
 }
 
