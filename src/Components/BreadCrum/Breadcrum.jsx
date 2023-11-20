ee06a10b3c99a1a5c62c29e11420c38fffaeec9a
umerfarooq6129 <umer.uze@gmail.com>
2023-11-20 23:36:26 +0500

    blank

--- src/Components/BreadCrum/Breadcrum.jsx
+++ src/Components/BreadCrum/Breadcrum.jsx
@@ -3,11 +3,12 @@ import './Breadcrum.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadcrum = (props) => {
-    const {product} = props;
+    // const {product} = props;
   return (
     <>
         <div className="breadcrum">
-            Home <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name} 
+            {/* Home <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}  */}
+            Home <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" /> <img src={arrow_icon} alt="" />
         </div>
     </>
   )
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ShopContext } from './../Context/ShopContext';
 import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/BreadCrum/Breadcrum';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <>
       <Breadcrum product={product} />
+      <ProductDisplay />
     </>
   )
 }
